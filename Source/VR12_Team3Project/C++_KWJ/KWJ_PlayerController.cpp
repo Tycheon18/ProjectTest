53be21489d84a15c4846e5b6cdc779b6b62e90df
tycheon18 <joong0818@gmail.com>
2023-05-22 20:34:52 +0900

    KWJ_Add World Timer

--- Source/VR12_Team3Project/C++_KWJ/KWJ_PlayerController.cpp
+++ Source/VR12_Team3Project/C++_KWJ/KWJ_PlayerController.cpp
@@ -42,22 +42,44 @@ void AKWJ_PlayerController::SetHUDCarriedAmmo(int32 Ammo)
 
 void AKWJ_PlayerController::SetHUDMatchCountdown(float CountdownTime)
 {
-	/*PlayerHUD = PlayerHUD == nullptr ? Cast<AKWJ_HUD>(GetHUD()) : PlayerHUD;
+	PlayerHUD = PlayerHUD == nullptr ? Cast<AKWJ_HUD>(GetHUD()) : PlayerHUD;
 	bool bHUDValid = PlayerHUD &&
-		PlayerHUD->CharacterOverlay &&
-		PlayerHUD->CharacterOverlay->MatchCountdownTest;
+		PlayerHUD->CharacterStateWidget &&
+		PlayerHUD->CharacterStateWidget->MatchCountdownText;
 	if (bHUDValid)
 	{
 		int32 Minutes = FMath::FloorToInt(CountdownTime / 60.f);
 		int32 Seconds = CountdownTime - (Minutes * 60);
 
-		FString CountdownText;
-		PlayerHUD->CharacterOverlay->ScoreAmount->SetText(FText::FromString(ScoreText));
-	}*/
+		FString CountdownText = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+		PlayerHUD->CharacterStateWidget->MatchCountdownText->SetText(FText::FromString(CountdownText));
+	}
 }
 
 void AKWJ_PlayerController::OnPossess(APawn* InPawn)
 {
+}
+
+void AKWJ_PlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetHUDTime();
+	CheckTimeSync(DeltaTime);
+}
+
+float AKWJ_PlayerController::GetServerTime()
+{
+	return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+}
+
+void AKWJ_PlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+	if (IsLocalController())
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+	}
 }
 
 void AKWJ_PlayerController::BeginPlay()
@@ -66,4 +88,42 @@ void AKWJ_PlayerController::BeginPlay()
 
 	PlayerHUD = Cast<AKWJ_HUD>(GetHUD());
 
+}
+
+
+
+void AKWJ_PlayerController::SetHUDTime()
+{
+	uint32 SecondsLeft = FMath::CeilToInt(MatchTime -GetServerTime());
+	if (CountdownInt != SecondsLeft)
+	{
+		SetHUDMatchCountdown(MatchTime - GetServerTime());
+	}
+
+
+	CountdownInt = SecondsLeft;
+}
+
+void AKWJ_PlayerController::CheckTimeSync(float DeltaTime)
+{
+
+	TimeSyncRunningTime += DeltaTime;
+	if (IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+		TimeSyncRunningTime = 0.f;
+	}
+}
+
+void AKWJ_PlayerController::ClientReportServerTime_Implementation(float TimeofClientRequest, float TimeServerReceivedClientRequest)
+{
+	float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeofClientRequest;
+	float CurrentServerTime = TimeServerReceivedClientRequest + (0.5f * RoundTripTime);
+	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+}
+
+void AKWJ_PlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest)
+{
+	float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+	ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
 }
